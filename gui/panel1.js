e07cf0a66f29c30a6bd3b675cd4aa014c8f4c873
Jakob Möderl <j.moederl@student.tugraz.at>
2017-02-16 22:43:09 +0100

    [enh] updated series storage format, remove button now has an "undo" function

--- gui/panel1.js
+++ gui/panel1.js
@@ -5,24 +5,14 @@ function $ (selector, el) {
     return el.querySelector(selector);
 }
 
-function addSeries(series, insertAtTop){
-    //check if insertAtTop parameter was given (=make it a default parameter)
-    if(typeof(insertAtTop) === 'undefined'){
-        insertAtTop = true;
-        id = 0;
-    }
+function addSeries(series){
 
-    var el;
-    //append new div with series to '#serien' panel
-    if(insertAtTop) {
-        el = $("#serien").insertBefore(create(series), $("#serien").firstChild);
-    } else {
-        el = $("#serien").appendChild(create(series));
-    }
+    var el = $("#serien").appendChild(create(series));
+
 
     //series button action
     el.addEventListener('click', function(){
-        browser.tabs.create({'url': series.lastEpisode.url});
+        browser.tabs.create({'url': series.url});
     });
 
     //"remove" button action
@@ -33,57 +23,34 @@ function addSeries(series, insertAtTop){
         removeSeries(el);
     });
 
-    //edit button action
-    var season = el.querySelector("#season");
-    var episode = el.querySelector("#episode");
-    season.addEventListener('click', function(e){e.stopPropagation()});
-    episode.addEventListener('click', function(e){e.stopPropagation()});
-
-    el.querySelector("#edit").addEventListener("click", function(e) {
-        //stop click event so the browser doesn't open the watchUrl in a new tab
-        e.stopPropagation();
-
-        season.removeAttribute("disabled");
-        episode.removeAttribute("disabled");
-    });
-    season.addEventListener('keydown', function(e) {
-        //keyCode 13 == enter --> update episode in storage
-        if(e.keyCode == 13 && parseInt(season.getAttribute('value')) && parseInt(episode.getAttribute('value'))) {
-            season.setAttribute("disabled", "true");
-            episode.setAttribute("disabled", "true");
-        }
-    });
-    episode.addEventListener('keydown', function(e) {
-        if(e.keyCode == 13 && parseInt(season.getAttribute('value')) && parseInt(episode.getAttribute('value'))) {
-            season.setAttribute("disabled", "true");
-            episode.setAttribute("disabled", "true");
-        }
-    });
-
-
     //remove the text that is displayed if no series was added
     if($("#noseries")){
         $("#noseries").remove();
     }
 }
 
+//create a DOM elemnt out of a HTML string
 function html2dom(string){
     var temp = document.createElement("template");
     temp.innerHTML = string;
     return temp.content.firstChild;
 }
 
+//create DOM elemnt for a series
 function create(series){
-    var str = '<div class="serie" id="' + series.id + '"><img src="' + series.img + '" class="titleImg"></img>' +
-        '<span class="seriesText"><img src="' + series.hostIcon + '" class="hostImg"></img><span class="title">' + series.name + '</span><br>' +
-        '<span class="status">S</span><input type="text" id="season" disabled="true" size="1" value="' + series.lastEpisode.season +'" />' +
-        '<span class="status">E</span><input type="text" id="episode" disabled="true" size="1" value="' + series.lastEpisode.epsiode +'" /><br>' +
-        '<img src="' + series.lngIcon + '" class="lngImg"></img>' +
-        '<span class="edit" id="edit" >Edit</span>' +
-        '<span class="remove" id="remove" >Remove</span>' +
-        '</span></div>';
+    var str = '<div class="series" id="' + series.id + '">' +
+        '<div id="remove">Remove</div>' +
+        '<img class="titleImg" src="' + series.img + '">'+
+        '<div class="seriesText">' +
+        '<span class="titleText">' + series.name + '</span><br>' +
+        '<span class="episodeNumber">S' + series.season + ' E' + series.episode + '</span><br>' +
+        '<span class="episodeTitle">' + series.episodeName + '</span><br>' +
+        '<span class="info"><img class="hostIcon" src="' + series.hostIcon + '"><img class="lngIcon" src="' + series.lngIcon + '"></span>' +
+        '</div>' +
+        '</div>';
     return html2dom(str);
 }
+
 
 function removeSeries(el){
     browser.storage.local.get().then(function (result) {
@@ -91,20 +58,20 @@ function removeSeries(el){
         var i;
         for(i=0; i<storageSeries.length; i++){
             if(el.id === storageSeries[i].id.toString()){
-
-                storageSeries.splice(i, 1);
-                var result = browser.storage.local.set({'series': storageSeries});
-                result.then(function () {
-
-                }, function (error) {
-                    console.log(JSON.stringify(error));
-                });
-                //Remove div from panel
+                var series = storageSeries[i];
                 el.remove();
 
-                if($('#serien').childElementCount === 0){
-                    $('#serien').appendChild(html2dom('<span id="noseries" class="noseries">episodes watched will be added automatically</span>'));
-                }
+                //add "undo" message
+                $('#serien').appendChild(html2dom('<span id="seriesRemoved">Removed ' + series.name + '. <span id="undo">undo</span>'));
+                $('#undo').addEventListener('click', function () {
+                   $('#seriesRemoved').remove();
+                   addSeries(series);
+                   storageSeries.push(series);
+                   browser.storage.local.set({'series': storageSeries});
+                });
+
+                storageSeries.splice(i, 1);
+                browser.storage.local.set({'series': storageSeries});
 
                 return;
             }
@@ -118,9 +85,11 @@ function removeSeries(el){
 //Get all series from storage and add them to the panel
 browser.storage.local.get().then(function (result) {
     var storageSeries = result.series;
-    if (!storageSeries) {
-        storageSeries = initTestSeries();
-        browser.storage.local.set({"series": storageSeries});
+
+    //if  no series are in storage, add text
+    if(storageSeries.length === 0){
+        $('#serien').appendChild(html2dom('<span>when you watch a episode it will get added automatically'));
+        return;
     }
 
     //add all series from storage to panel
@@ -129,46 +98,3 @@ browser.storage.local.get().then(function (result) {
         addSeries(storageSeries[i], i);
     }
 });
-
-
-
-
-function initTestSeries(){
-    return [
-        {
-            "id": 2,
-            "url": "http://onwatchseries.to/serie/reign",
-            "img:": "http://static.onwatchseries.to/uploads/thumbs/11/11293-reign.jpg",
-            "lngIcon": "../icons/en.png",
-            "hostIcon": "http://static.onwatchseries.to/templates/default/images/favicon.ico",
-            "lastEpisode": {
-                "season": 1,
-                "epsiode": 2,
-                "url": "http://onwatchseries.to/episode/reign_s1_e2.html"
-            },
-            "newestEpisode": {
-                "season": 1,
-                "episode": 2,
-                "url": "http://onwatchseries.to/episode/reign_s1_e2.html"
-            }
-
-        }, {
-            "id": 1,
-            "name": "Doctor Who",
-            "url": "http://onwatchseries.to/serie/doctor_who_2005",
-            "img": "http://static.onwatchseries.to/uploads/thumbs/13/13311-doctor_who_2005.jpg",
-            "lngIcon": "../icons/en.png",
-            "hostIcon": "http://static.onwatchseries.to/templates/default/images/favicon.ico",
-            "lastEpisode": {
-                "season": 10,
-                "epsiode": 0,
-                "url": "http://onwatchseries.to/episode/doctor_who_2005_s10_e0.html"
-            },
-            "newestEpisode": {
-                "season": 10,
-                "episode": 0,
-                "url": "http://onwatchseries.to/episode/doctor_who_2005_s10_e0.html"
-            }
-        }
-    ];
-}
